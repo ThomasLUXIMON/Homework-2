e140b5e0c89435ff2faf33b792466159acf26e24
Thomas LUXIMON <tluximon24@student.oulu.fi>
2024-03-11 17:10:58 +0200

    scripts added

--- Assets/Scripts/Hand.cs
+++ Assets/Scripts/Hand.cs
@@ -1,18 +1,56 @@
+using System
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+[RequireComponent(typeof(Animator))]
 public class Hand : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private float speed;
+    Animator animator;
+    private float gripTarget;
+    private float triggerTarget;
+    private float gripCurrent;
+    private float triggerCurrent;
+    private string animatorGripParam = "Grip";
+    private string animatorTriggerParam = "Trigger";
+    
+
+
     void Start()
     {
-        
+        animator = GetComponent<animator>()
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
-        
+        AnimateHand();
+    }
+
+    internal void SetGrip(float v)
+    {
+        gripTarget = v;
+    }
+
+    internal void SetGrip(float v)
+    {
+        triggerTarget = v;
+    }
+
+    void AnimateHand()  
+    {
+       if (gripCurrent != gripTarget)
+        {
+            gripCurrent = Mathf.MoveTowards(gripCurrent, gripTarget, Time.deltaTime * speed)
+            animator.SetFloat(animatorGripParam, gripCurrent)
+        }
+      
+        if (triggerCurrent != triggerTarget)
+        {
+            triggerCurrent = Mathf.MoveTowards(triggerCurrent, triggerTarget, Time.deltaTime * speed)
+            animator.SetFloat(animatorGripParam, triggerCurrent)
+        }
     }
 }
